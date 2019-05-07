d108040a353fb18e5ce6584e4b2851f8049df5c3
Mauricio <Mauricio@DESKTOP-GDE8RGT>
2019-05-07 02:50:53 -0300

    Prueba de github

--- src/pasteleria/Pasteleria.java
+++ src/pasteleria/Pasteleria.java
@@ -15,7 +15,7 @@ public class Pasteleria {
      * @param args the command line arguments
      */
     public static void main(String[] args) {
-        System.out.println("Hola");
+        System.out.println("Hola Mundo");
     }
     
 }
